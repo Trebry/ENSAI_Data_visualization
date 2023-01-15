7904d6fe675ae152bdd62e1ca80e81a0aeca68cd
Otis COOPER <otis.william.cooper@gmail.com>
2023-01-15 12:15:28 +0100

    Add lost and found

--- ui.R
+++ ui.R
@@ -10,10 +10,17 @@ library(tidyverse)
 library(geojsonR)
 library(tidytransit)
 
+# Download data using API
+#stations_raw <- read_delim("https://ressources.data.sncf.com/api/v2/catalog/datasets/referentiel-gares-voyageurs/exports/csv?limit=-1&offset=0&timezone=UTC",delim = ";", escape_double = FALSE, trim_ws = TRUE)
+#horaires_gare <- read_delim("https://ressources.data.sncf.com/api/v2/catalog/datasets/horaires-des-gares1/exports/csv?limit=-1&offset=0&timezone=UTC",delim = ";", escape_double = FALSE, trim_ws = TRUE)
+#routeinfo <- read_delim("./mess/tarifs-tgv-inoui-ouigo.csv",delim=";")
+routeinfo <- read_delim("https://ressources.data.sncf.com/api/v2/catalog/datasets/tarifs-tgv-inoui-ouigo/exports/csv?limit=-1&offset=0&timezone=UTC",delim=";")
+lost_found <- read_delim("https://ressources.data.sncf.com/api/v2/catalog/datasets/objets-trouves-restitution/exports/csv?order_by%3Ddate%20asc&limit=100&offset=0&timezone=UTC",delim = ";", escape_double = FALSE, trim_ws = TRUE)
+gtfs <- read_gtfs("https://eu.ftp.opendatasoft.com/sncf/gtfs/export_gtfs_voyages.zip")
+
+# Loading data that shouldn't change
 stations_raw <- read_delim("mess/referentiel-gares-voyageurs.csv",delim = ";", escape_double = FALSE, trim_ws = TRUE)
 horaires_gare<- read_delim("mess/horaires-des-gares1.csv",delim = ";", escape_double = FALSE, trim_ws = TRUE)
-routeinfo <- read_delim("./mess/tarifs-tgv-inoui-ouigo.csv",delim=";")
-gtfs <- read_gtfs("https://eu.ftp.opendatasoft.com/sncf/gtfs/export_gtfs_voyages.zip")
 
 
 shinyUI(
@@ -33,7 +40,10 @@ shinyUI(
              # Find your train
              source(file = "ui_page4.R", local = T)$value,
              
+             # Lost and found
+             source(file = "ui_page5.R", local = T)$value,
+             
              # About this site
-             source(file = "ui_page5.R", local = T)$value
+             source(file = "ui_page6.R", local = T)$value
   )
 )
--- ui_page5.R
+++ ui_page5.R
@@ -1,21 +1,18 @@
-# Home page
-tabPanel("About this site",
-         tags$div(
-           tags$h4("About"), 
-           "This application was developped by Otis Cooper Stein Dijkstra during their master's for Smart Data Science at ENSAI for the course of Data Visualization", 
-
-           tags$br(),
-           tags$br(),
-           
-           tags$h4("Code"), 
-           "The code is available on ",
-           tags$a(href="https://github.com/Trebry/ENSAI_Data_visualization", "github"),
-           
-           tags$br(),
-           tags$br(),
-           
-           tags$h4("Data"), 
-           "All the data we used is avaiable on the ",
-           tags$a(href="https://ressources.data.sncf.com/pages/accueil/", "SNCF website")
+# Lost and found
+tabPanel("Lost and found",
+         sidebarLayout(
+           sidebarPanel(
+             # selection of the variable
+             checkboxGroupInput(inputId = "lost_col", label = "Select the data you want to show :", 
+                                choices = unique(lost_found$gc_obo_type_c), 
+                                selected = unique(lost_found$gc_obo_type_c))
+           ),
+         mainPanel(
+            tabsetPanel(
+              tabPanel("Most recent items lost", dataTableOutput("lostTable")),
+              tabPanel("Most commun items lost (pie chart)", plotOutput("PiePlot")),
+              tabPanel("Most commun items lost (bar chart)", plotOutput("DistPlot"))
+            )
          )
+    )
 )
